2088dafdf5b4d8f4e4891d85cff6000fb0828a7c
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-04-19 23:30:32 +0300

    textGame

---
+++ TextBasedAdventureGame/textGame.py
@@ -0,0 +1,12 @@
+#Now let’s see how to create a text-based adventure game with Python
+name = str(input("Enter Your Name: "))
+print(f"{name} you are stuck at work")
+print(" You are still working and suddenly you you saw a ghost, Now you have two options")
+print("1.Run. 2.Jump from the window")
+user = int(input("Choose from the list (1 or 2): "))
+if user == 1:
+    print("You did it")
+elif user == 2:
+    print("You are not that type of smart")
+else:
+    print("Please Check your input")
